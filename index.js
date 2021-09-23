1a173e71270584e530df62537c009bb9786208b7
Octacilio Almeida <octa.oca44@gmail.com>
2021-09-23 16:09:57 -0300

    inicio

---
+++ index.js
@@ -0,0 +1,25 @@
+class Filme {
+    constructor(nome, linkImagem, site){
+        this.nome = nome;
+        this.linkImagem = linkImagem;
+        this.site = site;
+    }
+
+    getNome(){
+        return this.nome;
+    }
+
+    getLinkImagem(){
+        return this.linkImagem;
+    }
+
+    getSite(){
+        return this.site;
+    }
+}
+
+var filmes = [{
+    nome: "frgrgg",
+    linkImagem:"rfvtyju",
+    site: "ferfr"
+}];
