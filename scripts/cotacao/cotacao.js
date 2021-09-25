7e74d5a68b32be45c0b313219b31d578da590a3a
Octacilio Carvalho de Almeida <octa.oca44@gmail.com>
2021-09-25 19:28:26 -0300

    cotacao

--- scripts/cotacao/cotacao.js
+++ scripts/cotacao/cotacao.js
@@ -1,10 +1,38 @@
-function troca(){
+function troca() {
 
     var val_destino = document.getElementById("val_destino").value;
     var val_origem = document.getElementById("val_origem").value;
 
     document.getElementById("val_origem").value = val_destino;
     document.getElementById("val_destino").value = val_origem;
+}
+
+function getCotacao() {
+    var val_destino = document.getElementById("val_destino").value;
+    var val_origem = document.getElementById("val_origem").value;
+
+    var tipo = `${val_origem}${val_destino}`
+
+    var url = `https://economia.awesomeapi.com.br/last/${val_origem}-${val_destino}`
+
+    var request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.onerror = function (e) {
+        document.getElementById('resultado').innerHTML = 'API IS OFFLine!'
+    }
+
+    request.onload = () => {
+        var response = JSON.parse(request.responseText)
+
+        if (response.erro) {
+            document.getElementById('resultado').innerHTML = 'Não foi possível Converter';
+        } else {
+            var data = response;
+            //document.getElementById('resultado').innerHTML = `<p>Cotacao: ${data.$.tipo.bid}</p>`
+            document.getElementById('resultado').innerHTML = `<p>Cotacao: ${data.$.tipo.bid}</p>`
+        }
+    }
+    request.send();
 }
 
 function converter() {
