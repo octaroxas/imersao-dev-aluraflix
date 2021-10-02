c3a0a7ae47ee3ddaebd831bb71cbf92c3b04178f
Octacilio Carvalho de Almeida <octa.oca44@gmail.com>
2021-10-02 19:44:24 -0300

    portfolio

---
+++ index.js
@@ -0,0 +1,24 @@
+function mudaTema() {
+
+    var cardPerfil = document.getElementById('card-perfil')
+    var projetos = document.getElementById('projetos');
+
+    cardPerfil.classList.toggle('light')
+    projetos.classList.toggle('light')
+
+    // if (cardPerfil.classList) {
+    //     cardPerfil.classList.add("light");
+    // }
+    // else {
+    //     elemento.className += " light";
+    // }
+
+    // for (var classe in cardPerfil.classList) {
+    //     if (classe == 'light') {
+    //         cardPerfil.classList.remove(classe);
+    //     }
+    //     else {
+    //         cardPerfil.classList.add('light');
+    //     }
+    // }
+}
